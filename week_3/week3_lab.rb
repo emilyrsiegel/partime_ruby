7fe1707d1f931b46a332497cda506b440c50cafa
emilyrsiegel <emilysiegel97@gmail.com>
2016-01-24 19:56:15 -0600

    Updated leapyear_1 method

--- week_3/week3_lab.rb
+++ week_3/week3_lab.rb
@@ -61,12 +61,11 @@ def leapyear_1
 	puts "Tell me an ending year"
 	ending_year=gets.chomp.to_i
 while starting_year <= ending_year
-	if starting_year % 4 == 0
+	if starting_year % 4 == 0 && starting_year % 100 != 0
 		puts starting_year
-	elsif starting_year % 100 == 0
 	elsif starting_year % 400 == 0
 		puts starting_year
-		
+				
 	end
 
 	starting_year = starting_year + 1
