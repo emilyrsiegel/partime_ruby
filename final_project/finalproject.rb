fd0dde1c75655abdc6b21570d91c5c469badadb5
emilyrsiegel <emilysiegel97@gmail.com>
2016-02-22 21:49:56 -0600

    Final Project Teen Application

--- final_project/finalproject.rb
+++ final_project/finalproject.rb
@@ -23,62 +23,65 @@ class Teen_Application
 			end
 		end
 
-		def application_id
-			@applicationid = rand(36**8).to_s(36).upcase
-			puts "Your application id is #{@applicationid}"
-		end
+		#generate applicaiton id. Used to log back into application at later time.
+	def application_id
+		@applicationid = rand(36**8).to_s(36).upcase
+		puts "Your application id is #{@applicationid}"
+	end
 
-		def existing_application
-			puts "Please enter your Application ID"
-			existing_appid = gets.chomp
+		#If already started an application, then enter their application id.
+	def existing_application
+		puts "Please enter your Application ID"
+		existing_appid = gets.chomp
 			if existing_appid == appid
 				puts "Welcome back! You must begin by entering your basic information"
 				enter_basic
 			else
 				puts "We do not have that Application ID on file. Please try again."
 			end
-		end
+	end
 
-		def enter_basic
-			puts "Step 1: Please enter your personal information below."
-			puts "First Name: "
-			@first_name = gets.chomp
-			puts "Last Name: "
-			@last_name = gets.chomp
-			puts "Birthdate (mm/dd/yyyy)"
-			@birth_date = gets.chomp.to_i
-			puts "Gender: Female, Male, Choose Not To Identify"
-			@gender = gets.chomp.capitalize
-			puts "Do you have a CPS ID? Y or N"
-			@cpsid = gets.chomp.capitalize
-				if @cpsid == "Y"
-					puts "Enter your CPS ID (8 digits)"
-					@cpsid_number = gets.chomp
-				end
-			puts "Please select your school: CPS High School, Private School, Elementary School, Home School, Suburban High School, College"
-				@school = gets.chomp
-				if @school == "CPS High School"
-					puts "Please enter the CPS high school you attend."
-					@cpsschool = gets.chomp
-				elsif @school == "Private School"
-				else
-					ineligible
-				end
-			puts "Please enter your grade"
-			@grade = gets.chomp.to_i
-				if @grade == 9 || @grade == 10 || @grade == 11 || @grade == 12
+	def enter_basic
+		puts "Step 1: Please enter your personal information below."
+		puts "First Name: "
+		@first_name = gets.chomp
+		puts "Last Name: "
+		@last_name = gets.chomp
+		puts "Birthdate (mm/dd/yyyy)"
+		@birth_date = gets.chomp.to_i
+		puts "Gender: Female, Male, Choose Not To Identify"
+		@gender = gets.chomp.capitalize
+		puts "Do you have a CPS ID? Y or N"
+		@cpsid = gets.chomp.capitalize
+			if @cpsid == "Y"
+				puts "Enter your CPS ID (8 digits)"
+				@cpsid_number = gets.chomp
+			end
+		puts "Please select your school: CPS High School, Private School, Elementary School, Home School, Suburban High School, College"
+		@school = gets.chomp
+			if @school == "CPS High School"
+				puts "Please enter the CPS high school you attend."
+				@cpsschool = gets.chomp
+			elsif @school == "Private School"
 			else
 				ineligible
-				end
-			puts "Do you live in Chicago? Y or N"
-			@live_chicago = gets.chomp.capitalize
-				if @live_chicago == "Y"
+			end
+		puts "Please enter your grade"
+		@grade = gets.chomp.to_i
+			if @grade == 9 || @grade == 10 || @grade == 11 || @grade == 12
+			else
+				ineligible
+			end
+		puts "Do you live in Chicago? Y or N"
+		@live_chicago = gets.chomp.capitalize
+			if @live_chicago == "Y"
 				search
-				else
-					ineligible
-				end
-		end
+			else
+				ineligible
+			end
+	end
 
+	#search for programs based on content area and/or model type
 	def search
 		puts "Step 2: Find Programs. Use the search filters to narrow your search of programs. Would you like to search by Content Area, Model Type, or Both?"
 		search_filter = gets.chomp
@@ -97,9 +100,9 @@ class Teen_Application
 				@model_type = gets.chomp, "\n"
 				program_options
 			end
-		end
+	end
 
-
+	#displays program options that fit applicant's search criteria and eligibility
 	def program_options
 		puts "Please see below for your program options:", "\n"
 			if @content_area == "Arts"
@@ -141,7 +144,7 @@ class Teen_Application
 				search
 			end
 
-		# data_collect
+		data_collect
 		end
 
 
@@ -167,12 +170,12 @@ class Teen_Application
 		exit
 	end 
 
-	private
+	private #exisiting applicationid
 	def appid
 		appid = "E2C2R48S"
 	end
 
-def data_collect
+def data_collect #store data on firebase
 		base_uri = 'https://teenappruby.firebaseio.com/'
 		firebase = Firebase::Client.new(base_uri)
 		response = firebase.push("todos", { :application_id => @applicationid, :first_name => @first_name, :last_name => @last_name, :birth_date => @birth_date, :gender => @gender, :cpsid => @cpsid, :school => @school, :cpsschool => @cpsschool, :cpsid_number => @cpsid_number, :grade => @grade, :live_chicago => @live_chicago, :content_area_search => @content_area, :model_type_search => @model_type})
